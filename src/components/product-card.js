3f65dfff91d2a5bf70081e87d947427962f3916a
ByeBye-Sama <joaquin.byebye@gmail.com>
2020-07-14 21:44:30 -0500

    feat: show product cards

---
+++ src/components/product-card.js
@@ -0,0 +1,75 @@
+import React from 'react'
+import { upperCase } from 'lodash'
+import {
+  makeStyles,
+  Card,
+  CardActions,
+  CardContent,
+  CardMedia,
+  Button,
+  Typography,
+  Box,
+  Divider
+} from '@material-ui/core'
+import EditIcon from '@material-ui/icons/Edit'
+import DeleteIcon from '@material-ui/icons/Delete'
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    display: 'flex',
+    flexDirection: 'column',
+    height: '100%',
+    justifyContent: 'space-between',
+    minWidth: theme.spacing(35)
+  },
+  media: {
+    height: theme.spacing(18)
+  },
+  category: {
+    color: '#909090',
+    fontWeight: 'bold'
+  }
+}))
+
+const ProductCard = props => {
+  const { name, description, category, price } = props
+
+  const classes = useStyles()
+
+  return (
+    <Card className={classes.root}>
+      <Box>
+        <CardMedia
+          className={classes.media}
+          image="https://images.photowall.com/products/59504/mountain-view-1.jpg?h=699&q=85"
+          title="Product"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" align="right">
+            $ {price.toFixed(2)}
+          </Typography>
+          <Divider />
+          <Typography variant="caption" className={classes.category}>
+            {upperCase(category)}
+          </Typography>
+          <Typography gutterBottom variant="h5" component="h2">
+            {name}
+          </Typography>
+          <Typography variant="body2" color="textSecondary" component="p">
+            {description}
+          </Typography>
+        </CardContent>
+      </Box>
+      <CardActions>
+        <Button color="primary" startIcon={<EditIcon />}>
+          Edit
+        </Button>
+        <Button color="secondary" startIcon={<DeleteIcon />}>
+          Delete
+        </Button>
+      </CardActions>
+    </Card>
+  )
+}
+
+export default ProductCard
--- src/views/home/index.js
+++ src/views/home/index.js
@@ -1,12 +1,62 @@
-import React from 'react'
-import { Button, Box } from '@material-ui/core'
+import React, { useState, useEffect } from 'react'
+import { map } from 'lodash'
+import { CircularProgress, Grid, Box, makeStyles } from '@material-ui/core'
+import { ProductCard } from 'components'
+import { getProducts } from './services'
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1,
+    height: '100%',
+    padding: theme.spacing(3)
+  }
+}))
 
 const HomeScreen = () => {
+  const classes = useStyles()
+
+  const [isLoading, setIsLoading] = useState(true)
+
+  const [products, setProducts] = useState([])
+
+  useEffect(() => {
+    const loadProducts = async () => {
+      const response = await getProducts()
+
+      if (response.status === 200) {
+        setProducts(response.data.products)
+
+        setIsLoading(false)
+      }
+    }
+
+    loadProducts()
+  }, [])
+
+  if (isLoading) {
+    return <CircularProgress size={60} />
+  }
+
+  const renderProducts = product => {
+    const { id, name, description, category, price } = product
+
+    return (
+      <Grid item xs key={id}>
+        <ProductCard
+          name={name}
+          description={description}
+          category={category}
+          price={price}
+        />
+      </Grid>
+    )
+  }
+
   return (
-    <Box>
-      <Button variant="contained" color="primary">
-        Home
-      </Button>
+    <Box className={classes.root} height="100%" flexGrow="1" padding={3}>
+      <Grid container spacing={3}>
+        {map(products, renderProducts)}
+      </Grid>
     </Box>
   )
 }
