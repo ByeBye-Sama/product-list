5632e101fddf403eaeb213d5783cfea383a98797
ByeBye-Sama <joaquin.byebye@gmail.com>
2020-07-16 21:24:31 -0500

    Updates

---
+++ precache-manifest.b5cc53645883fad0fa2a0dece3ecadd6.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c78605235cb31c2bb99c4e56db55cc30",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7c22062a6ec3de19cdf4",
+    "url": "/static/js/0.0586b77d.chunk.js"
+  },
+  {
+    "revision": "40fca01342bd136a78ab",
+    "url": "/static/js/3.1c26ddc3.chunk.js"
+  },
+  {
+    "revision": "bddaf2d2dd30806dcb3350bb7877b1d9",
+    "url": "/static/js/3.1c26ddc3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec1606af65360566e6d1",
+    "url": "/static/js/4.2035ea65.chunk.js"
+  },
+  {
+    "revision": "97a32f03e2186e5c9bdf",
+    "url": "/static/js/5.c233da40.chunk.js"
+  },
+  {
+    "revision": "986455e05beeeac3c9a2",
+    "url": "/static/js/6.359ad0f2.chunk.js"
+  },
+  {
+    "revision": "9e7c5fe23092cb5d3167",
+    "url": "/static/js/main.355be3b4.chunk.js"
+  },
+  {
+    "revision": "16f50e32be4ad1333fb3",
+    "url": "/static/js/runtime-main.b37601a8.js"
+  }
+]);
